19b091748d7ca266aa34fb904d00bd5abd8d75e7
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-10 10:39:18 -0600

    Setup Storyboard and Subclass ViewControllers and Custom Cell

---
+++ CollectionViews/CollectionViews/StateCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StateCollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Bradley GIlmore on 5/10/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class StateCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CollectionViews/CollectionViews/StateCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StateCollectionViewController.swift
+//  CollectionViews
+//
+//  Created by Bradley GIlmore on 5/10/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class StateCollectionViewController: UICollectionViewController {
+
+
+    
+}
